9fc3cfe9fbb2aa78d5d31384fb391d79d87adba8
HuiVisby <44123696+HuiVisby@users.noreply.github.com>
2026-03-04 16:26:37 +0100

    Update mart_google_trends_regional.sql

--- nordic_retail/models/marts/mart_google_trends_regional.sql
+++ nordic_retail/models/marts/mart_google_trends_regional.sql
@@ -11,10 +11,19 @@
           when 'FI' then 'Finland'
       end                         as country_name,
       region,
-      keyword,
+      keyword,  
       case keyword
-          when 'snus'   then 'Snus'
-          when 'nuuska' then 'Snus'
+          when 'snus'             then 'Snus'
+          when 'nuuska'           then 'Snus'
+          when 'nikotinpåsar'     then 'Nicotine Pouches'
+          when 'nikotinposer'     then 'Nicotine Pouches'
+          when 'nikotiinipussit'  then 'Nicotine Pouches'
+          when 'ZYN'              then 'Nicotine Pouches'
       end                         as product_category,
+
+      case keyword
+          when 'ZYN' then 'ZYN Brand'
+          else 'Category Search'
+      end                         as search_type,
       search_interest
   from {{ ref('stg_google_trends_nicotine_by_region') }}
