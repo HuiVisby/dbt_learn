8ef17e37cad01dfa38339efe4be776be809dc105
HuiVisby <44123696+HuiVisby@users.noreply.github.com>
2026-03-04 16:13:28 +0100

    Create mart_google_trends_regional.sql

---
+++ nordic_retail/models/marts/mart_google_trends_regional.sql
@@ -0,0 +1,20 @@
+ {{
+      config(materialized='table')
+  }}
+
+  select
+      country_code,
+      case country_code
+          when 'SE' then 'Sweden'
+          when 'NO' then 'Norway'
+          when 'DK' then 'Denmark'
+          when 'FI' then 'Finland'
+      end                         as country_name,
+      region,
+      keyword,
+      case keyword
+          when 'snus'  then 'Snus'
+          when 'nuuska' then 'Snus'
+      end                         as product_category,
+      regional_interest
+  from {{ ref('stg_google_trends_nicotine_by_region') }}
