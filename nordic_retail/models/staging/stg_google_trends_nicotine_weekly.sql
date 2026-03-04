29884286d3392b341d40fa1f23c660dc25aab383
HuiVisby <44123696+HuiVisby@users.noreply.github.com>
2026-03-04 16:05:19 +0100

    Update stg_google_trends_nicotine_weekly.sql

--- nordic_retail/models/staging/stg_google_trends_nicotine_weekly.sql
+++ nordic_retail/models/staging/stg_google_trends_nicotine_weekly.sql
@@ -17,22 +17,4 @@
   )
 
   select * from cleaned
-  EOF
-
-  cat > ~/dbt_learn/nordic_retail/models/staging/stg_google_trends__nicotine_by_region.sql << 'EOF'
-  with source as (
-      select * from {{ source('raw_ingest', 'google_trends_nicotine_by_region') }}
-  ),
-
-  cleaned as (
-      select
-          country_code,
-          region,
-          keyword,
-          search_interest,
-          ingested_at
-      from source
-      where search_interest > 0
-  )
-
-  select * from cleaned
+  
